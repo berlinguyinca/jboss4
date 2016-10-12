fe53ccb82d23d2585017d45fcef8d7a2759a96af
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-10-12 11:21:18 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 echo "my host is: ${PUBLIC_HOSTNAME}"
-${JBOSS_HOME}/bin/run.sh -b 0.0.0.0 -Djava.rmi.server.hostname=${PUBLIC_HOSTNAME} ${JBOSS_OPTIONS}
+${JBOSS_HOME}/bin/run.sh -b 0.0.0.0 -Djava.rmi.server.hostname=${PUBLIC_HOSTNAME} -Djava.rmi.server.useLocalHostname=false
+ ${JBOSS_OPTIONS}
